35c42789049412feb2d8915e8858b309d27af206
eNricoC5 <cruzenrico28@gmail.com>
2017-10-04 16:50:47 -0700

    Robotics stuff

---
+++ Operators/src/OperatorPractice.java
@@ -0,0 +1,12 @@
+
+public class OperatorPractice {
+	public static void main(String[] args) {
+		int a = 1;
+		double x = 2.0;
+		boolean y = false;
+		char c = 'c';
+		String s = "Eat me";
+		System.out.println(s + " " + c);
+	}
+
+}
